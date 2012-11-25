5e348e8ed0ea11e115b352bd38dde0c95f9cedae
Ricardo <Ricardo@berkley-blue>
2012-11-25 17:25:41 +0000

    general

--- insereArtigoBD.php
+++ insereArtigoBD.php
@@ -13,6 +13,7 @@ $dataTempo = $date->format('Y-m-d H:i:s') . "\n";
 
 
 $con = mysql_connect("a.db.shared.orchestra.io","user_b0b749ab","(@H@zi0vv4&ewj");
+
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
