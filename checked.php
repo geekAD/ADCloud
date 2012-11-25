7e1fb360220fc1c1dac9a54758aac2ee1bdfdc1f
Ricardo <Ricardo@berkley-blue>
2012-11-25 17:31:50 +0000

    general

--- checked.php
+++ checked.php
@@ -8,12 +8,12 @@ $dados = $_POST['op'];
 $id = preg_replace("/[^0-9]/", "", $dados);
 
 
-if(isset($_GET['delete'])){
+if(($_GET['delete'])){
 mysql_query("DELETE FROM artigos WHERE artigos.id=$id");
 var_dump($id);
 echo "delete";
 //header("location: gerirArtigos.php?numPag=".$_SESSION['numPag']);
-}else if(isset($_GET['edit'])){
+}else if(($_GET['edit'])){
     var_dump($id);
     echo "edit";
     
