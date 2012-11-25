74b212f57b45fbff555524a0cd8b101d0494aedf
Ricardo <Ricardo@berkley-blue>
2012-11-25 16:33:51 +0000

    BD

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 
 session_start();
-mysql_connect('localhost','user_b0b749ab','(@H@zi0vv4&ewj');
+mysql_connect('a.db.shared.orchestra.io','user_b0b749ab','(@H@zi0vv4&ewj');
 mysql_select_db('a.db.shared.orchestra.io');
 
 ?>
