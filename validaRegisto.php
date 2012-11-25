2485e2063b1914687c9937c8f652c6512d7aea9c
Ricardo <Ricardo@berkley-blue>
2012-11-25 16:48:22 +0000

    BD

--- validaRegisto.php
+++ validaRegisto.php
@@ -4,13 +4,13 @@ $username = $_POST['username'];
 $password = md5($_POST['password']);
 $nome = $_POST['nome'];
 
-$con = mysql_connect("localhost","user_b0b749ab","(@H@zi0vv4&ewj");
+$con = mysql_connect("a.db.shared.orchestra.io","user_b0b749ab","(@H@zi0vv4&ewj");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
 
-mysql_select_db("a.db.shared.orchestra.io", $con);
+mysql_select_db("db_b0b749ab", $con);
 
 mysql_query("INSERT INTO utilizadores (email, password, nome)
 VALUES ('".$username."', '".$password."','".$nome."')");
