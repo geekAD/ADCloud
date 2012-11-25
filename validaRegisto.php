70260ed6b1d748c62f50c73b96b7238ff9badf21
Ricardo <Ricardo@berkley-blue>
2012-11-25 16:37:42 +0000

    BD

--- validaRegisto.php
+++ validaRegisto.php
@@ -4,7 +4,7 @@ $username = $_POST['username'];
 $password = md5($_POST['password']);
 $nome = $_POST['nome'];
 
-$con = mysql_connect("https://mysql.orchestra.io/index.php?db=db_b0b749ab&token=ba4b62c5b6b68686eff2d41638bed125#PMAURL:db=db_b0b749ab&server=1&target=db_structure.php&token=ba4b62c5b6b68686eff2d41638bed125","user_b0b749ab","(@H@zi0vv4&ewj");
+$con = mysql_connect("https://mysql.orchestra.io/","user_b0b749ab","(@H@zi0vv4&ewj");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
